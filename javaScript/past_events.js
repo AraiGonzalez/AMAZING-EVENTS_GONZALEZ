b8792478e1b0d94341bc4aec9f39d195974c803a
AraiGonzalez <araiestefania94@hotmail.com>
2023-03-08 17:34:41 -0300

    finish task2

---
+++ javaScript/past_events.js
@@ -0,0 +1,25 @@
+const contenedorCards = document.getElementById('elemento')
+
+showCards(data.events,contenedorCards)
+
+
+function showCards(arrayDatos,contenedor){
+  let cards = ''
+  let pastDate = data.currentDate
+  for(datas of arrayDatos){
+   
+    if (pastDate>datas.date){
+    cards += `<div class="card contCard" style="width: 18rem;">
+    <img src="${datas.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title"> ${datas.name}</h5>
+      <p class="card-text">${datas.description}</p>
+      <p>${datas.price}</p>
+      <a href="/details.html#cinema" class="btn btn-primary"> Datails</a>
+    </div>
+  </div>`
+     } ;
+  }
+  contenedor.innerHTML = cards
+  
+}
